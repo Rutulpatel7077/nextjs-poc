d407a6ad3ec0a1acfdb5f9b3acb61cdb61a5b96b
Rutul Patel <rutulpatel7077@gmail.com>
2022-03-19 18:19:58 -0300

    Added home page

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,5 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export default function middleware(req: NextRequest) {
-  return NextResponse.rewrite('/strange');
+  const url = req.nextUrl.clone();
+  try {
+    // Get hostname (e.g. vercel.com, test.vercel.app, etc.)
+    const hostname = req.headers.get("host");
+    // If localhost, assign the host value manually
+    // If prod, get the custom domain/subdomain value by removing the root URL
+    // (in the case of "test.vercel.app", "vercel.app" is the root URL)
+    const currentHost = hostname?.replace(":3000", "");
+
+    url.pathname = `/_sites/${currentHost}${url.pathname}`;
+    return NextResponse.rewrite(url);
+  } catch (err) {
+    console.error(err);
+    url.pathname = '/strange';
+    return NextResponse.rewrite(url);
+  }
 }
