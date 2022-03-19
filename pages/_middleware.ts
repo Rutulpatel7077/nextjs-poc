c9c0b3ae9a83e0f4cc56eb4b7acd36fccca6f43a
Rutul Patel <rutulpatel7077@gmail.com>
2022-03-19 18:17:57 -0300

    Removed path name

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export default function middleware(req: NextRequest) {
-  return NextResponse.rewrite('/test-404');
+  return NextResponse.rewrite('/strange');
 }
