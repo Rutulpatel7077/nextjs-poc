ae3960c2b7a7eabdc7e2a84569e4626b299dabfc
Rutul Patel <rutulpatel7077@gmail.com>
2022-03-21 21:49:38 -0300

    test middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,39 +1,30 @@
-// import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-// export default function middleware(req: NextRequest) {
-//   const url = req.nextUrl.clone();
-//   // Get hostname (e.g. vercel.com, test.vercel.app, etc.)
-//   const hostname = req.headers.get("host");
+export default function middleware(req: NextRequest) {
+  const url = req.nextUrl.clone();
+  // Get hostname (e.g. vercel.com, test.vercel.app, etc.)
+  const hostname = req.headers.get("host");
 
-//   // If localhost, assign the host value manually
-//   // If prod, get the custom domain/subdomain value by removing the root URL
-//   // (in the case of "test.vercel.app", "vercel.app" is the root URL)
-//   const currentHost = hostname?.replace(':3000', '') || "hello";
+  // If localhost, assign the host value manually
+  // If prod, get the custom domain/subdomain value by removing the root URL
+  // (in the case of "test.vercel.app", "vercel.app" is the root URL)
+  const currentHost = hostname?.replace(':3000', '') || "hello";
 
-//   // Prevent security issues – users should not be able to canonically access
-//   // the pages/sites folder and its respective contents. This can also be done
-//   // via rewrites to a custom 404 page
-//   if (url.pathname.startsWith(`/_sites`)) {
-//     return new Response(null, { status: 404 });
-//   }
+  // Prevent security issues – users should not be able to canonically access
+  // the pages/sites folder and its respective contents. This can also be done
+  // via rewrites to a custom 404 page
+  if (url.pathname.startsWith(`/_sites`)) {
+    return new Response(null, { status: 404 });
+  }
 
-//   console.log(currentHost, "currentHost");
+  console.log(currentHost, "currentHost");
 
-//   if (
-//     !url.pathname.startsWith("/api") // exclude all API routes
-//   ) {
-//     // rewrite to the current hostname under the pages/sites folder
-//     // the main logic component will happen in pages/sites/[site]/index.tsx
-//     url.pathname = `/_sites/${currentHost}${url.pathname}`;
-//     return NextResponse.rewrite(url);
-//   }
-// }
-
-import type { NextFetchEvent, NextRequest } from 'next/server';
-
-export default function middleware(
-  request: NextRequest,
-  event: NextFetchEvent,
-) {
-  return new Response('Hello, world!');
+  if (
+    !url.pathname.startsWith("/api") // exclude all API routes
+  ) {
+    // rewrite to the current hostname under the pages/sites folder
+    // the main logic component will happen in pages/sites/[site]/index.tsx
+    url.pathname = `/_sites/${currentHost}${url.pathname}`;
+    return NextResponse.rewrite(url);
+  }
 }
