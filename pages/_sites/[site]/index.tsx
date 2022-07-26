31e2380040dcf2d18d6b2e396be53248704c15fa
Rutul Patel <rutulpatel7077@gmail.com>
2022-07-26 06:20:38 -0300

    Fixed

--- pages/_sites/[site]/index.tsx
+++ pages/_sites/[site]/index.tsx
@@ -1,8 +1,36 @@
 import type { NextPage } from "next";
+import Script from "next/script";
+
 const Index: NextPage = () => {
   return (
-    <div >
-      <h1>Index site Page</h1>
+    <div>
+      <div id="my-reform"></div>
+
+      <Script
+        id="reform-script-1"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html:
+            "window.Reform=window.Reform||function(){(Reform.q=Reform.q||[]).push(arguments)};",
+        }}
+      />
+      <Script
+        id="reform-script"
+        async
+        src="https://embed.reform.app/v1/embed.js"
+      ></Script>
+      <Script
+        id="reform-script-2"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          Reform('init', {
+              url: 'https://forms.reform.app/Hx5lKz/untitled-form/ey7nc9',
+              target: '#my-reform',
+              background: 'transparent',
+          })`,
+        }}
+      />
     </div>
   );
 };
